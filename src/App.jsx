f55fe51bef2cabb457c8ee81be878db2e412f778
DESKTOP-3LOI9O7\Lime <answjdgus72@gmail.com>
2025-09-23 11:01:47 +0900

    초기설정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Login from './pages/login/Login.JSX'
+function App() {
+ 
+
+  return (
+   <BrowserRouter>
+      <Routes>
+       <Route path="/login" element={<Login />} /> 
+      </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App;
