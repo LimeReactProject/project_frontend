6477f1e6f8ef54b4c37ae7673ed4b0f7d4c094b8
yjyj0234 <yjyj0345@naver.com>
2025-09-25 14:41:36 +0900

    0925손

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login/Login.JSX'
 import EntityList from './components/EntityList';
+import Reservation from './components/reservation/reservation.jsx';
+
 function App() {
  
 
@@ -10,6 +12,8 @@ function App() {
       <Routes>
        <Route path="/login" element={<Login />} /> 
        <Route path="/" element={<EntityList/>}/>
+       <Route path="/reserv" element={<Reservation/>} />
+       
       </Routes>
    </BrowserRouter>
   )
